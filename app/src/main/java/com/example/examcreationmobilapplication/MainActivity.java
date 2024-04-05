92b7bc3acca6823ce8d995a6b24084e2a01ee087
Efnan Kadıoğlu <118856046+efnankadioglu@users.noreply.github.com>
2024-04-05 23:27:23 +0300

    15.38 de kaldım

--- app/src/main/java/com/example/examcreationmobilapplication/MainActivity.java
+++ app/src/main/java/com/example/examcreationmobilapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.examcreationmobilapplication;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,26 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Objects;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    TextView textLevel;
+    TextView textRightAnswered;
+    TextView textQuestion;
+
+
+    Button buttonOp1;
+    Button buttonOp2;
+    Button buttonOp3;
+
+    int level = 0;
+    int great = 0;
+    int rightAnswer = 0;
+    String realOperation = "";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +41,54 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        textLevel = findViewById(R.id.textQuestionNumber);
+        textRightAnswered = findViewById(R.id.textRightAnswered);
+        textQuestion = findViewById(R.id.textQuestion);
+
+        buttonOp1 = findViewById(R.id.buttonOptional1);
+        buttonOp2 = findViewById(R.id.buttonOptional2);
+        buttonOp3 = findViewById(R.id.buttonOptional3);
+
+        textLevel.setText("Q : "+ level+" / 5");
+        textRightAnswered.setText("RA"+ great + " / 5");
+
+        if(level < 5){
+            getARandomQuestion();
+        }
+
+
+    }
+
+    private void getARandomQuestion() {
+
+        buttonOp1.setBackgroundResource(R.drawable.buttons_option_bg);
+        buttonOp2.setBackgroundResource(R.drawable.buttons_option_bg);
+        buttonOp3.setBackgroundResource(R.drawable.buttons_option_bg);
+
+
+        int firstNumber = new Random().nextInt(20);
+        int secondNumber = new Random().nextInt(20);
+
+
+        int operation = new Random().nextInt(3) + 1;
+
+        int operationA = new Random().nextInt(100);
+        int operationB = new Random().nextInt(100);
+
+
+
+
+        // if kısmında kaldım
+
+
+
+
+
+
     }
 }
